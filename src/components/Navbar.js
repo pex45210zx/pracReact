1cba6687e17b58c632b580e18664ccaa8e790f51
Siravat matarach <pex_der@outlook.co.th>
2024-03-13 20:24:17 +0700

    clone public file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    
+                    <Link className='nav-logo'>
+                    </Link>
                 </div>
             </nav>
         </>
