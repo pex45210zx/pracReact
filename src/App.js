ec0309f2a9f944ce861ba797a0a545f0a41dbc7a
Siravat matarach <pex_der@outlook.co.th>
2024-03-13 15:25:02 +0700

    commit from branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
